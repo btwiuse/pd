b2ace632a2cb260149c2a84c11c75eb03c2d30ce
navigaid <navigaid@gmail.com>
2019-12-11 14:20:15 +0000

    implement ParallelDownloader

--- pd.go
+++ pd.go
@@ -1,19 +1,129 @@
 package main
 
-import(
-	"log"
+import (
 	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"strings"
 )
 
-func main(){
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan(){
-		line := strings.TrimSpace(scanner.Text())
-		if len(line) == 0 {
+func main() {
+	config := parseFlags()
+	pd := New(config.Jobs, config.Template)
+	go pd.Scan(os.Stdin)
+	pd.Run()
+}
+
+// Push blocks when q is full
+func (q Qlock) Push() {
+	q <- struct{}{}
+}
+
+func (q Qlock) Pop() {
+	<-q
+}
+
+type Qlock chan struct{}
+
+// Factory manufactores Job
+func (j *Factory) Job(id string) *Job {
+	return &Job{
+		id: id,
+		url: fmt.Sprintf(j.template, id),
+	}
+}
+
+type Factory struct {
+	template string
+	// timeout time.Duration
+}
+
+// Job downloads url
+func (j *Job) Run() {
+	resp, err := http.Get(j.url)
+	if err != nil {
+		log.Println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(j.id, string(body))
+}
+
+type Job struct {
+	id string
+	url string
+}
+
+// Config holds command line flags
+func parseFlags() *Config {
+	config := &Config{}
+	flag.StringVar(&config.Template, "t", "", "url template, like https://example.com/%s.json")
+	flag.IntVar(&config.Jobs, "j", 3, "parallel jobs")
+	flag.Parse()
+	return config
+}
+
+type Config struct {
+	Jobs     int
+	Template string
+}
+
+// ParallelDownloader wraps Qlock and Factory
+func New(j int, t string) *ParallelDownloader {
+	return &ParallelDownloader{
+		in:      make(chan string),
+		out:     make(chan interface{}),
+		Qlock:   make(chan struct{}, j),
+		Factory: &Factory{t},
+	}
+}
+
+type ParallelDownloader struct {
+	in  chan string
+	out chan interface{}
+	Qlock
+	*Factory
+}
+
+func (p *ParallelDownloader) Run() {
+	for {
+		select {
+		case id := <-p.in:
+			// https://dave.cheney.net/2014/03/19/channel-axioms
+			// A receive from a closed channel returns the zero value immediately
+			// which implies io.EOF from Scan
+			if id == "" {
+				return
+			}
+			p.Push()
+			go func() {
+				defer p.Pop()
+				p.Job(id).Run()
+			}()
+			/*
+				case <-p.ctx.Done():
+					log.Println("context done")
+					return
+			*/
+		}
+	}
+}
+
+func (p *ParallelDownloader) Scan(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		id := strings.TrimSpace(scanner.Text())
+		if len(id) == 0 {
 			continue
 		}
-		log.Println(line)
+		p.in <- id
 	}
+	close(p.in)
 }
